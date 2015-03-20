91724a5e8d28e6f947d23d248f4788efce80d24a
Alexandre Toyer <alexandre.toyer@open-searh-server.com>
2015-03-20 08:54:32 +0100

    Add authentification for queries, update ResponseFactory

--- src/OpenSearchServer/Response/ResponseFactory.php
+++ src/OpenSearchServer/Response/ResponseFactory.php
@@ -7,58 +7,50 @@ class ResponseFactory
 {
     public static function createResponse(BuzzResponse $response, \OpenSearchServer\Request $request)
     {
-        switch(get_class($request)) {
-            case 'OpenSearchServer\Search\Pattern\Search':
-            case 'OpenSearchServer\Search\Field\Search':
+        if(            is_a($request, 'OpenSearchServer\Search\Pattern\Search')
+                    || is_a($request, 'OpenSearchServer\Search\Field\Search')) {
                 return new \OpenSearchServer\Response\SearchResult($response, $request);
-                break;
-            case 'OpenSearchServer\Monitor\Monitor':
+        } else if (    is_a($request, 'OpenSearchServer\Monitor\Monitor')) {
                 return new \OpenSearchServer\Response\ResponseMonitor($response, $request);
-                break;
-            case 'OpenSearchServer\Autocompletion\Query':
+        } else if (    is_a($request, 'OpenSearchServer\Autocompletion\Query')) {
                 $response = new ResponseIterable($response, $request);
                 if(!empty($response->getJsonValues()->terms)) {
                     $response->setValues($response->getJsonValues()->terms);
                 }
                 return $response;
-                break;
-            case 'OpenSearchServer\Index\GetList':
+		} else if (	   is_a($request, 'OpenSearchServer\Index\GetList')) {
                 $response = new ResponseIterable($response, $request);
                 if(!empty($response->getJsonValues()->indexList)) {
                     $response->setValues($response->getJsonValues()->indexList);
                 }
                 return $response;
-                break;
-            case 'OpenSearchServer\Field\GetList':
+		} else if (		is_a($request, 'OpenSearchServer\Field\GetList')) {
                 $response = new ResponseIterable($response, $request);
                 if(!empty($response->getJsonValues()->fields)) {
                     $response->setValues($response->getJsonValues()->fields);
                 }
                 return $response;
-                break;
-            case 'OpenSearchServer\Analyzer\GetList':
+		} else if (    is_a($request, 'OpenSearchServer\Analyzer\GetList')) {
                 $response = new ResponseIterable($response, $request);
                 if(!empty($response->getJsonValues()->analyzers)) {
                     $response->setValues($response->getJsonValues()->analyzers);
                 }
                 return $response;
-                break;
-            case 'OpenSearchServer\Crawler\Web\Patterns\Exclusion\GetList':
-            case 'OpenSearchServer\Crawler\Web\Patterns\Inclusion\GetList':
-            case 'OpenSearchServer\Autocompletion\GetList':
-            case 'OpenSearchServer\Synonyms\GetList':
-            case 'OpenSearchServer\Crawler\Rest\GetList':
-            case 'OpenSearchServer\Replication\GetList':
-            case 'OpenSearchServer\Parser\GetList':
-            case 'OpenSearchServer\StopWords\GetList':
-            case 'OpenSearchServer\Crawler\File\Repository\GetList':
+		} else if (	   is_a($request, 'OpenSearchServer\Crawler\Web\Patterns\Exclusion\GetList')
+                    || is_a($request, 'OpenSearchServer\Crawler\Web\Patterns\Inclusion\GetList')
+                    || is_a($request, 'OpenSearchServer\Autocompletion\GetList')
+                    || is_a($request, 'OpenSearchServer\Synonyms\GetList')
+                    || is_a($request, 'OpenSearchServer\Crawler\Rest\GetList')
+                    || is_a($request, 'OpenSearchServer\Replication\GetList')
+                    || is_a($request, 'OpenSearchServer\Parser\GetList')
+                    || is_a($request, 'OpenSearchServer\StopWords\GetList')
+                    || is_a($request, 'OpenSearchServer\Crawler\File\Repository\GetList')) {
                 $response = new ResponseIterable($response, $request);
                 if(!empty($response->getJsonValues()->items)) {
                     $response->setValues($response->getJsonValues()->items);
                 }
                 return $response;
-                break;
-            case 'OpenSearchServer\Spellcheck\GetList':
+         } else if (   is_a($request, 'OpenSearchServer\Spellcheck\GetList')) {
                 $response = new ResponseIterable($response, $request);
                 $values = $response->getJsonValues();
                 if(!empty($values)) {
@@ -69,43 +61,38 @@ class ResponseFactory
                     $response->setValues($templates);
                 }
                 return $response;
-                break;
-            case 'OpenSearchServer\SearchTemplate\GetList':
-            case 'OpenSearchServer\MoreLikeThis\GetList':
+         } else if (   is_a($request, 'OpenSearchServer\SearchTemplate\GetList')
+                    || is_a($request, 'OpenSearchServer\MoreLikeThis\GetList')) {
                 $response = new ResponseIterable($response, $request);
                 if(!empty($response->getJsonValues()->templates)) {
                     $response->setValues($response->getJsonValues()->templates);
                 }
                 return $response;
                 break;
-            case 'OpenSearchServer\Synonyms\Get':
-            case 'OpenSearchServer\StopWords\Get':
+         } else if (   is_a($request, 'OpenSearchServer\Synonyms\Get')
+                    || is_a($request, 'OpenSearchServer\StopWords\Get')) {
                 $response = new ResponseIterable($response, $request);
                 $content = $response->getRawContent();
                 if(!empty($content)) {
                     $response->setValues(explode("\n", $content));
                 }
                 return $response;
-                break;
-            //Synonyms\Exists has a particular behaviour: existence is based on HTTP response code (200 / 404)
-            case 'OpenSearchServer\Synonyms\Exists':
-            case 'OpenSearchServer\StopWords\Exists':
+         }
+         //Synonyms\Exists has a particular behaviour: existence is based on HTTP response code (200 / 404)
+         else if (	   is_a($request, 'OpenSearchServer\Synonyms\Exists')
+                    || is_a($request, 'OpenSearchServer\StopWords\Exists')) {
                 $headers = $response->getHeaders();
                 $responseHttpCode = $headers[0];
                 $response = new \OpenSearchServer\Response\Response($response, $request);
                 $response->setSuccess(strpos($responseHttpCode, '200 OK') !== false);
                 return $response;
-                break;
-            case 'OpenSearchServer\SpellCheck\Search':
+         } else if (   is_a($request, 'OpenSearchServer\SpellCheck\Search')) {
                 return new \OpenSearchServer\Response\SpellCheckResult($response, $request);
-                break;
-            case 'OpenSearchServer\MoreLikeThis\Search':
+         } else if (   is_a($request, 'OpenSearchServer\MoreLikeThis\Search')) {
                 return new \OpenSearchServer\Response\MoreLikeThisResult($response, $request);
-                break;
-            case 'OpenSearchServer\SearchBatch\SearchBatch':
+         } else if (   is_a($request, 'OpenSearchServer\SearchBatch\SearchBatch')) {
                 return new \OpenSearchServer\Response\SearchBatchResult($response, $request);
-                break;
-            default:
+         } else {
                 return new \OpenSearchServer\Response\Response($response, $request);
         }
     }    
